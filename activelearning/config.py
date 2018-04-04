72e98d010ecb9c785cdf6489e4f7107afd8c559d
Efthymios Tzinis <etzinis@gmail.com>
2018-04-04 11:26:34 +0300

    Configuration file for paths to be changed

---
+++ activelearning/config.py
@@ -0,0 +1,15 @@
+import os
+
+BASE_PATH = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+EMOBASE_PATH = '/home/george/extracted_features/linear/SAVEE_linear_emobase2010'
+NL_FEATURE_PATH = '/home/george/extracted_features/utterance'
+
+PYUNICORN_PATH = '/home/thymios/Desktop/Research/pyunicorn/'
+SAVEE_PATH = '/home/thymios/Research/datasets/SAVEE/'
+IEMOCAP_PATH = '/home/thymios/Desktop/Research/datasets/IEMOCAP/'
+BERLIN_PATH = '/home/thymios/Desktop/Research/datasets/emodb/'
+EXTRACTED_FEATURES_PATH = '/home/thymios/Desktop/Research/data/'
+OPENSMILE_CONFIG_PATH = '/home/thymios/Desktop/Research/opensmile-2.3' \
+                        '.0/config/emobase2010.conf'
+
